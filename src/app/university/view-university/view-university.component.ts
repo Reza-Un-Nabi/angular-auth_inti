264029f714f06ba004e05ad9996fe01818cd5404
nahid.imtiaz <nahid.imtiaz@robi.com.bd>
2020-07-08 23:26:19 +0600

    add drop down

--- src/app/university/view-university/view-university.component.ts
+++ src/app/university/view-university/view-university.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
+
+
+import { CountryService } from '../../_services/module_service/country.service';
+import { Country } from '../../models/country';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-view-university',
@@ -7,9 +14,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUniversityComponent implements OnInit {
 
-  constructor() { }
+  countryViewForm: FormGroup;
+  countries: Observable<Country[]>;
+//   countries = ['USA', 'Canada', 'Uk']
+  constructor(private fb: FormBuilder,
+  private countryService: CountryService
+  ) {}
 
   ngOnInit() {
+   this.countryViewForm = this.fb.group({
+     countryControl: ['Canada']
+   });
+   this.getAllCountryList();
   }
+
+  getAllCountryList(): void {
+      this.countryService.getAllCountry().subscribe(data=>{
+          this.countries = data;
+      });
+
+    }
 
 }
